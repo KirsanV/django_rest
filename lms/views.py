366e03eedd5e8fcccba4eb4ab39f494018fb8eb0
KirsanV <kirsanshalkhakov@yahoo.com>
2025-10-03 09:47:43 +0300

    hm30.2

--- lms/views.py
+++ lms/views.py
@@ -1,8 +1,10 @@
 from rest_framework import viewsets
 from .models import Course, Lesson
-from .serializers import CourseSerializer, LessonSerializer
+from .serializers import CourseSerializer, LessonSerializer, PaymentSerializer
 from rest_framework import generics
-
+from users.models import Payment
+from rest_framework import filters
+from django_filters.rest_framework import DjangoFilterBackend
 
 class CourseViewSet(viewsets.ModelViewSet):
     queryset = Course.objects.all()
@@ -17,3 +19,15 @@ class LessonListCreate(generics.ListCreateAPIView):
 class LessonRetrieveUpdateDestroy(generics.RetrieveUpdateDestroyAPIView):
     queryset = Lesson.objects.all()
     serializer_class = LessonSerializer
+
+
+class PaymentViewSet(viewsets.ModelViewSet):
+    queryset = Payment.objects.all()
+    serializer_class = PaymentSerializer
+
+    filter_backends = [DjangoFilterBackend, filters.OrderingFilter]
+    filterset_fields = ['user', 'course', 'lesson', 'payment_method']
+    ordering_fields = ['payment_date', 'amount']
+    ordering = ['-payment_date']
+
+
